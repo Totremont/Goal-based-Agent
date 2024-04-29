0515d06ea27161059e78bbcf80541a9e93ff1ad8
Ezequiel M. Izaguirre <ezequielmizaguirre@gmail.com>
2024-04-29 19:25:04 -0300

    Añadidas las clases e instancias para formar el mapa

--- src/amongus/models/Room.java
+++ src/amongus/models/Room.java
@@ -10,26 +10,43 @@ import java.util.List;
 public class Room 
 {
     private final String name;
-    private List<Room> nextTo;
+    private List<Room> neighbors = new ArrayList<>(5);  //[O,N,E,S,T]
+    
     private final RoomType type;
     private final Sabotage sabotage;
-    
-    
-    public Room(String name, List<Room> nextTo, RoomType type , Sabotage sabotage) {
-        this.name = name;
-        this.nextTo = nextTo;
-        this.type = type;
-        this.sabotage = sabotage;
-    }
+    private final RoomState state;
 
-    public Room(String name, RoomType type, Sabotage sabotage) {
+    public Room(String name, RoomType type, Sabotage sabotage) 
+    {
         this.name = name;
         this.type = type;
         this.sabotage = sabotage;
     }
+    
+    public Room neighborAt(Cardinal cardinal)
+    {
+        return neighbors.get(cardinal.ordinal());
+    }
 
-    public void setNextTo(List<Room> nextTo) {
-        this.nextTo = nextTo;
+    //Añade un nuevo vecino y le notifico al mismo que me añada a mi
+    public void addNeighbor(Room room, Cardinal cardinal)
+    {
+        if(!room.equals(neighbors.get(cardinal.ordinal()))) //todo - No hay logica de reemplazo de vecino
+        {
+            this.neighbors.add(cardinal.ordinal(), room);
+            room.addMe(this, opposite(cardinal));
+        }
+        else System.out.printf("El vecino %s ya existe en %s en dirección %s",room.getName(), this.getName(), cardinal);
+    }
+    
+    //Cuando un vecino me pide a mi que lo añada
+    void addMe(Room room, Cardinal cardinal)
+    {
+        if(!this.neighbors.contains(room))
+        {
+            this.neighbors.add(room);
+        }
+        else System.out.printf("El vecino %s ya existe en %s",room.getName(), this.getName());
     }
     
        
@@ -38,7 +55,22 @@ public class Room
         SECCION, PASILLO, TUBERIA
     }
     
-    public class RoomState
+    //Dirección cardinal
+    public enum Cardinal
+    {
+        OESTE, NORTE,ESTE,SUR, TUB  //Se necesitarían más direcciones para añadir tuberias. - controlar esto
+    }
+    
+    //Obtiene la dirección opuesta a la dada
+    private Cardinal opposite(Cardinal cardinal)
+    {
+        if(cardinal.equals(Cardinal.TUB)) return Cardinal.TUB;
+        
+        int index = cardinal.ordinal() > 1 ? cardinal.ordinal() - 2 : cardinal.ordinal() + 2;
+        return Cardinal.values()[index];
+    }
+    
+    private class RoomState
     {
         private List<CrewMember> currentMembers;
         private Boolean agentPresent;
@@ -49,10 +81,23 @@ public class Room
             this.agentPresent = agentPresent;
             this.usedSabotages = new ArrayList<>();
         }
+        
+    }
+    
+    public String getName() {
+        return name;
+    }
 
-        
-        
-        
+    public List<Room> getNeighbors() {
+        return neighbors;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public Sabotage getSabotage() {
+        return sabotage;
     }
     
 }
